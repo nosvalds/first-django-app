049e805f98eb7a75522ab53f77d3e4addb99da0b
Nik Osvalds <nosvalds@gmail.com>
2020-07-16 15:53:51 +0100

    refactor view and update detail template

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 from django.template import loader
 from django.http import Http404
@@ -21,10 +21,13 @@ def about(request):
     return HttpResponse("Hello, world. You're at the polls about page.")
 
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
+    # full version of below which uses get_object_or_404 for simplicity
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404("Question does not exist")
+    # return render(request, 'polls/detail.html', {'question': question})
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
